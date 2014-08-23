6a696d3c2782474acd4fbda5f237cda7f2f61550
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2014-08-23 22:07:18 +0200

    FK import from string

--- client/js/controllers/gameCtrl.js
+++ client/js/controllers/gameCtrl.js
@@ -562,86 +562,90 @@ angular.module('vassalApp.controllers')
         };
 
         $scope.fk_read_result = [];
-        $scope.readFKList = function(file) {
+        $scope.fk_read_string = '';
+        function importFKList(data) {
+          // console.log(data);
+          var lines = data.match(/[^\r\n]+/g);
+          $scope.create_preview.info = [];
+          // console.log(lines);
+          var i = 0;
+          var global_offset_x = 0;
+          _.each(lines, function(line) {
+            if(line.match(/^(System:|Faction:|Casters:|Points:|Tiers:)/)) {
+              return;
+            }
+            line = line.replace(/^\s*/,'');
+            if(line.length === 0) return;
+            line = line.replace(/^\* /,'');
+            var size = 1;
+            var match = line.match(/^(\d+) /);
+            if(match) {
+              size = match[1] >> 0;
+              line = line.replace(/^\d+ /,'');
+            }                
+            match = line.match(/\(leader and (\d+) grunts?\)/i);
+            if(match) {
+              size = (match[1] >> 0) + 1;
+              line = line.replace(/\(leader and (\d+) grunts?\)/gi,'');
+            }                
+            line = line.replace(/\s*\(.+\)\s*$/,'');
+            // console.log(line);
+            if(_.isArray($scope.factions.fk_keys[line]) &&
+               $scope.factions.fk_keys[line].length > 0) {
+              // console.log(size);
+
+              if($scope.factions.fk_keys[line].length > 1) {
+                _.each($scope.factions.fk_keys[line], function(id) {
+                  $scope.create_preview.info.push({
+                    info: id,
+                    offset_x: global_offset_x + 1.25*$scope.factions.fk_keys[line][0].r,
+                    offset_y: 0
+                  });
+                  global_offset_x += 2.5*$scope.factions.fk_keys[line][0].r;
+                  i++;
+                });
+              }
+              else {
+                var mid_size = Math.ceil(size/2);
+                var unit_step = 2.5*$scope.factions.fk_keys[line][0].r;
+                var max_offset_x = 0;
+                _.times(size, function(n) {
+                  var offset_x = 0;
+                  var offset_y = 0;
+                  if(size <= 5) {
+                    offset_x = n*unit_step+unit_step/2;
+                    offset_y = 0;
+                  }
+                  else {
+                    offset_x = (i%mid_size)*unit_step+unit_step/2;
+                    offset_y = (n >= mid_size) ? unit_step : 0;
+                  }
+                  max_offset_x = Math.max(max_offset_x, offset_x);
+                  $scope.create_preview.info.push({
+                    info: $scope.factions.fk_keys[line][0],
+                    offset_x: global_offset_x + offset_x,
+                    offset_y: offset_y
+                  });
+                  i++;
+                });
+                global_offset_x += max_offset_x + unit_step/2;
+              }
+            }
+            else {
+              $scope.fk_read_result.push('!!! unknown model \"'+line+'\"');
+            }
+          });
+          // console.log($scope.create_preview.info);
+          if(i > 0) $scope.create_mode = true;
+        }
+        $scope.readFKFile = function(file) {
           $scope.create_mode = false;
           $scope.fk_read_result = [];
           var reader = new $window.FileReader();
           reader.onload = function(e) {
             $scope.fk_read_result.push('loaded file');
             var data = e.target.result;
-            // console.log(data);
-            var lines = data.match(/[^\r\n]+/g);
-            $scope.create_preview.info = [];
-            // console.log(lines);
-            var i = 0;
-            var global_offset_x = 0;
-            _.each(lines, function(line) {
-              if(line.match(/^(System:|Faction:|Casters:|Points:|Tiers:)/)) {
-                return;
-              }
-              line = line.replace(/^\s*/,'');
-              if(line.length === 0) return;
-              line = line.replace(/^\* /,'');
-              var size = 1;
-              var match = line.match(/^(\d+) /);
-              if(match) {
-                size = match[1] >> 0;
-                line = line.replace(/^\d+ /,'');
-              }                
-              match = line.match(/\(leader and (\d+) grunts?\)/i);
-              if(match) {
-                size = (match[1] >> 0) + 1;
-                line = line.replace(/\(leader and (\d+) grunts?\)/gi,'');
-              }                
-              line = line.replace(/\s*\(.+\)\s*$/,'');
-              console.log(line);
-              if(_.isArray($scope.factions.fk_keys[line]) &&
-                 $scope.factions.fk_keys[line].length > 0) {
-                console.log(size);
-
-                if($scope.factions.fk_keys[line].length > 1) {
-                  _.each($scope.factions.fk_keys[line], function(id) {
-                    $scope.create_preview.info.push({
-                      info: id,
-                      offset_x: global_offset_x + 1.25*$scope.factions.fk_keys[line][0].r,
-                      offset_y: 0
-                    });
-                    global_offset_x += 2.5*$scope.factions.fk_keys[line][0].r;
-                    i++;
-                  });
-                }
-                else {
-                  var mid_size = Math.ceil(size/2);
-                  var unit_step = 2.5*$scope.factions.fk_keys[line][0].r;
-                  var max_offset_x = 0;
-                  _.times(size, function(n) {
-                    var offset_x = 0;
-                    var offset_y = 0;
-                    if(size <= 5) {
-                      offset_x = n*unit_step+unit_step/2;
-                      offset_y = 0;
-                    }
-                    else {
-                      offset_x = (i%mid_size)*unit_step+unit_step/2;
-                      offset_y = (n >= mid_size) ? unit_step : 0;
-                    }
-                    max_offset_x = Math.max(max_offset_x, offset_x);
-                    $scope.create_preview.info.push({
-                      info: $scope.factions.fk_keys[line][0],
-                      offset_x: global_offset_x + offset_x,
-                      offset_y: offset_y
-                    });
-                    i++;
-                  });
-                  global_offset_x += max_offset_x + unit_step/2;
-                }
-              }
-              else {
-                $scope.fk_read_result.push('!!! unknown model \"'+line+'\"');
-              }
-            });
-            console.log($scope.create_preview.info);
-            if(i > 0) $scope.create_mode = true;
+            importFKList(data);
             $scope.$apply();
           };
           reader.onerror = function(e) {
@@ -653,6 +657,9 @@ angular.module('vassalApp.controllers')
             $scope.$apply();
           };
           reader.readAsText(file);
+        };
+        $scope.readFKString = function(file) {
+          importFKList($scope.fk_read_string);
         };
       });
     }
