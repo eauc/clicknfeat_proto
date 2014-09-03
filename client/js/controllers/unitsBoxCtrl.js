614e748bce9dec29312cdef1e8f4cada9fe0e5c3
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2014-09-03 21:47:44 +0200

    improve units panel

--- client/js/controllers/unitsBoxCtrl.js
+++ client/js/controllers/unitsBoxCtrl.js
@@ -5,14 +5,11 @@ angular.module('vassalApp.controllers')
     '$scope',
     function($scope) {
       console.log('init unitsBoxCtrl');
-      $scope.units = [];
+      $scope.units = {};
       _.each($scope.game.models, function(model) {
         if(!model.state.unit) return;
-        $scope.units.push({
-          number: model.state.unit,
-          id: model.state.id,
-          info: model.info
-        });
+        $scope.units[model.state.unit] = $scope.units[model.state.unit] || [];
+        $scope.units[model.state.unit].push(model);
       });
     }
   ]);
