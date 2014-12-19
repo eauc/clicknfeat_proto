bd241628e726a63b816f9e6c1ba412a141d09c72
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2014-12-19 17:10:53 +0100

    custom mouse event listeners

--- client/js/directives/gameMove.js
+++ client/js/directives/gameMove.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('vassalApp.directives')
-  .directive('gameMove', function() {
+  .directive('boardMove', function() {
     return {
       restrict: 'A',
       controller: [
@@ -10,8 +10,53 @@ angular.module('vassalApp.directives')
         }
       ],
       link: function(scope, iElement, iAttrs) {
+        iElement[0].onmousedown = function(event) {
+          scope.doSelectStart(event);
+        };
         iElement[0].onmousemove = function(event) {
           scope.doSelectMove(event);
+        };
+        iElement[0].onmouseup = function(event) {
+          scope.doSelectStop(event);
+          scope.$apply();
+        };
+      }
+    };
+  })
+  .directive('modelMove', function() {
+    return {
+      restrict: 'A',
+      controller: [
+        '$scope',
+        function($scope) {
+        }
+      ],
+      link: function(scope, iElement, iAttrs) {
+        iElement[0].onmousedown = function(event) {
+          scope.onModelMouseDown(event, scope.model);
+        };
+        iElement[0].onmouseup = function(event) {
+          scope.doSelectStop(event, scope.model);
+          scope.$apply();
+        };
+      }
+    };
+  })
+  .directive('templateMove', function() {
+    return {
+      restrict: 'A',
+      controller: [
+        '$scope',
+        function($scope) {
+        }
+      ],
+      link: function(scope, iElement, iAttrs) {
+        iElement[0].onmousedown = function(event) {
+          scope.onTemplateMouseDown(event, scope.tp);
+        };
+        iElement[0].onmouseup = function(event) {
+          scope.doSelectStop(event);
+          scope.$apply();
         };
       }
     };
